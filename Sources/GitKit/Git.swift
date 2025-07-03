4ec4ee2c2304d2ed21f3e94c8e269d843158fd8d
Andrew McKnight <git@mcknight.rocks>
2025-07-03 15:32:39 -0800

    fix params; abbrev-ref is a boolean option; add separate param for the actual revision to parse

--- Sources/GitKit/Git.swift
+++ Sources/GitKit/Git.swift
@@ -38,7 +38,11 @@ public final class Git: Shell {
         case submoduleForeach(recursive: Bool = false, command: String)
         case renameRemote(oldName: String, newName: String)
         case addRemote(name: String, url: String)
-        case revParse(abbrevRef: String)
+
+        /// - parameter abbrevRef whether or not the result should be the abbreviated reference name or the full commit SHA hash
+        /// - parameter revision the name of the revision to parse. can be symbolic (`@`), human-readable (`origin/HEAD`) or a commit SHA hash
+        case revParse(abbrevRef: Bool, revision: String)
+
         case revList(branch: String, count: Bool = false, revisions: String? = nil)
         case raw(String)
         case lsRemote(url: String, limitToHeads: Bool = false)
@@ -154,10 +158,14 @@ public final class Git: Shell {
                 params.append(command)
             case .writeConfig(let name, let value):
                 params = [Command.config.rawValue, "--add", name, value]
-            case .revParse(abbrevRef: let abbrevRef):
-                params = [Command.revParse.rawValue, "--abbrev-ref", abbrevRef]
             case .readConfig(let name):
                 params = [Command.config.rawValue, "--get", name]
+            case .revParse(let abbrevRef, let revision):
+                params = [Command.revParse.rawValue]
+                if abbrevRef {
+                    params.append("--abbrev-ref")
+                }
+                params.append(revision)
             case .revList(let branch, let count, let revisions):
                 params = [Command.revList.rawValue]
                 if count {
